f586601cbb796d1288388aec07bfd0f2c50e7973
Clement Pang <clement@lirvanalabs.com>
2024-04-02 15:34:53 -0700

    add transcription example

---
+++ example/src/main/java/example/OpenAiApiTranscriptionExample.java
@@ -0,0 +1,22 @@
+package example;
+
+import io.github.panghy.openai.audio.CreateTranscriptionRequest;
+import io.github.panghy.openai.audio.TranscriptionResult;
+import io.github.panghy.openai.service.OpenAiService;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.time.Duration;
+
+public class OpenAiApiTranscriptionExample {
+  public static void main(String[] args) {
+    String token = System.getenv("OPENAI_TOKEN");
+    OpenAiService service = new OpenAiService(token, Duration.ofSeconds(30));
+
+    URL resource = OpenAiService.class.getResource("/hello-world.mp3");
+    TranscriptionResult transcription = service.createTranscription(CreateTranscriptionRequest.builder().
+        model("whisper-1").
+        build(), resource.getPath());
+    System.out.println(transcription);
+  }
+}
